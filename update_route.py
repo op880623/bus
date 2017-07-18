fea7b719101d706b48e1240b4db34b862f3c5939
op880623 <op880623@gmail.com>
2017-07-18 16:44:50 +0800

    divide into two parts

--- update_route.py
+++ update_route.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import re
 import shelve
 from datetime import datetime
@@ -13,10 +11,8 @@ from bus.models import BusRoute, BusStop
 logFile = open('update.log', 'a', encoding='utf8')
 
 dbRoute = 'route'
-dbStop = 'stop'
 
 routeData = shelve.open(dbRoute)
-stopData = shelve.open(dbStop)
 
 def log(text):
     print(text)
@@ -61,7 +57,7 @@ for routeId in routeIds:
 
     route = BeautifulSoup(page.text, "html.parser")
 
-    routeName = str(route.find(class_='stationlist-title').string)
+    routeName = re.search('<span class="stationlist-title">(.+)</span>', page.text).group(1)
 
 # check id and name
     try:
@@ -99,58 +95,5 @@ for routeId in routeIds:
 log('\nfinish update route time: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
 
-
-# update stops' infomation
-log('\nstart update stop time: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-
-# get all stops' id from all routes
-stopIds = set()
-for key in routeData.keys():
-    stopIds = stopIds.union(set(routeData[key].routeForward))
-    stopIds = stopIds.union(set(routeData[key].routeBackward))
-
-# update every stop
-for stopId in list(stopIds):
-    page = requests.get("https://ebus.gov.taipei/Stop/RoutesOfStop?Stopid=" + stopId)
-    if page.status_code != 200:
-        log('website of stop ' + stopId + ' is broken or not found.')
-        continue
-
-# check id and name
-    stopName = re.search('<p class="routelist-text">(.+)</p>', page.text).group(1)
-    try:
-        busStop = stopData[stopId]
-        if busStop.name != stopName:
-            log(busStop.name + ' is renamed to ' + stopName + '.')
-            busStop.name = stopName
-    except KeyError:
-        busStop = BusStop(id=stopId, name=stopName)
-        log(busStop.name + ' is created.')
-
-# check route pass stop
-    route = re.findall('"UniRouteId":"(\d+)"', page.text)
-    if busStop.route != route:
-        busStop.route = route
-        log(busStop.name + ' route is updated.')
-
-# check stop's location
-    latitude = float(re.search('"Latitude":(\d+.\d+)', page.text).group(1))
-    if busStop.latitude != latitude:
-        busStop.latitude = latitude
-        log(busStop.name + ' latitude is updated.')
-
-    longitude = float(re.search('"Longitude":(\d+.\d+)', page.text).group(1))
-    if busStop.longitude != longitude:
-        busStop.longitude = longitude
-        log(busStop.name + ' longitude is updated.')
-
-    stopData[busStop.id] = busStop
-    print(busStop.name + ' is updated.')
-
-log('\nfinish update stop time: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-
-
-
 routeData.close()
-stopData.close()
 logFile.close()
